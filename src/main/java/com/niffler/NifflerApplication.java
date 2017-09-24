ddd4fdff7e450da616b96a992a599c64e7032197
LAPTOP-D1GRJ4AO\banma <865641511@qq.com>
2017-09-24 17:26:28 +0800

    init
    init project

---
+++ src/main/java/com/niffler/NifflerApplication.java
@@ -0,0 +1,12 @@
+package com.niffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NifflerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NifflerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/niffler/NifflerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.niffler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NifflerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
